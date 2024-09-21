28021b5cb52f4e2a9ac4a86586054db176c80b12
jamoos <lirazkafri@gmail.com>
2024-09-21 10:29:19 +0300

    add printVersion task to build file

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,6 @@ plugins {
     id("com.github.johnrengelman.shadow") version "7.1.2"
 
     // // This will handle versioning - hopefully
-    // id("pl.allegro.tech.build.axion-release") version "1.14.2"
     id("dev.quiescence.plugins.versioning") version "0.1"
 	
 }
@@ -19,19 +18,15 @@ plugins {
 group = "com.ido"
 description = "HelloWorld"
 version = "1.0.0"
-//
-// scmVersion {
-//     tag.prefix("v")
-//     nextVersion {
-//       suffix("SNAPSHOT")
-//     }
-//     versionIncrementer("incrementPatch")
-//   }
-
 application.mainClass.set("com.ido.HelloWorld")
 tasks.jar {
     manifest {
         attributes["Main-Class"] = "com.ido.HelloWorld" 
+    }
+}
+tasks.register("printVersion") {
+    doLast {
+        println(project.version)
     }
 }
 repositories {
