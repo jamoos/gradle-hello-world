2200be7bef0907eef23c769bb428923c603173b4
jamoos <lirazkafri@gmail.com>
2024-09-22 12:43:38 +0300

    try to output version again

--- src/main/java/com/ido/HelloWorld.java
+++ src/main/java/com/ido/HelloWorld.java
@@ -1,4 +1,7 @@
 package com.ido;
+import java.util.jar.Manifest;
+import java.io.IOException;
+import java.net.URL;
 
 /**
  * Hello world!
@@ -6,7 +9,18 @@ package com.ido;
  
 public class HelloWorld {
     public static void main(String[] args) {
-        String version = HelloWorld.class.getPackage().getImplementationVersion();
-        System.out.println("Mr. K says: \"World, Hold on! (Version: " + version + ")\"");
+        try {
+            // Get the Manifest from the JAR file
+            URL manifestUrl = HelloWorld.class.getResource("/META-INF/MANIFEST.MF");
+            Manifest manifest = new Manifest(manifestUrl.openStream());
+
+            // Get the Implementation-Version attribute
+            String version = manifest.getMainAttributes().getValue("Implementation-Version");
+
+            System.out.println("Mr. K says: \"World, Hold on!\" (Version: " + version + ")");
+        } catch (IOException e) {
+            System.err.println("Error reading Manifest: " + e.getMessage());
+        }
     }
 }
+
