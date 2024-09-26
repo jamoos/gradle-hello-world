0c147846575c070998f1315a8a220f037d9d55e2
jamoos <lirazkafri@gmail.com>
2024-09-26 09:54:51 +0300

    run the pipeline

--- src/main/java/com/ido/HelloWorld.java
+++ src/main/java/com/ido/HelloWorld.java
@@ -6,6 +6,6 @@ package com.ido;
  
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Mr. K says: \"World, Hold on!\"");
+        System.out.println("Mr. K says: \"World, Dont Hold on!\"");
     }
 }
